36bd90963a83d209b959ea7a158c40c11fda0ba3
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-12-14 16:02:21 +0100

    tolto codice inutile

--- php/register.php
+++ php/register.php
@@ -14,32 +14,6 @@ $luogo_di_nascita = $connection->real_escape_string($_POST['luogo_di_nascita']);
 //password hashata
 $hash_password = password_hash($password, PASSWORD_DEFAULT);
 
-$error = '';
-
-if (empty($nome)) {
-    $error = "inserisci il nome";
-} 
-else if (empty($cognome)) {
-    $error = "inserisci il cognome";
-}
-else if (empty($email)) {
-    $error = "inserisci l' email";
-}
-else if (empty($password)) {
-    $error = "inserisci la password";
-}
-else if (empty($codice_Fiscale)) {
-    $error = "inserisci il codice fiscale";
-}
-else if (empty($sesso)) {
-    $error = "inserisci il sesso";
-}
-else if (empty($compleanno)) {
-    $error = "inserisci la tua data di nascita";
-}
-else if (empty($luogo_di_nascita)) {
-    $error = "inserisci il luogo di nascita";
-}
 
 $query = "INSERT INTO users (nome, cognome, email, password, codice_Fiscale, sesso, compleanno, luogo_di_nascita) 
         VALUES ('$nome', '$cognome', '$email', '$hash_password', '$codice_Fiscale', '$sesso', '$compleanno', '$luogo_di_nascita')";
